dab390eec383d48a2b1c256aa11127b0c2a03952
XiaoMing <1402609439@qq.com>
2022-05-19 10:43:55 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1111111111111");
+        System.out.println("2222222222222");
+        System.out.println("3333333333333");
+    }
+}
