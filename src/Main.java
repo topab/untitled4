97a2315c7f6367d5d9619beb869553931e879c61
XiaoMing <1402609439@qq.com>
2022-05-19 11:39:51 +0800

    12

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,39 @@
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1111111111111");
-        System.out.println("2222222222222");
-        System.out.println("3333333333333");
+        String content=getHttp("https://api.sdccx.cn");
+        System.out.println(content);
+    }
+
+    public static String getHttp(String get_url) {
+        StringBuffer stringBuffer = new StringBuffer();
+        try {
+            URL url = new URL(get_url);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setConnectTimeout(5 * 1000);//链接超时
+            urlConnection.setReadTimeout(5 * 1000);//返回数据超时
+            //getResponseCode (1.200请求成功 2.404请求失败)
+            if (urlConnection.getResponseCode() == 200) {
+                //获得读取流写入
+                InputStream inputStream = urlConnection.getInputStream();
+                byte[] bytes = new byte[1024];
+                int len = 0;
+                while ((len = inputStream.read(bytes)) != -1) {
+                    stringBuffer.append(new String(bytes, 0, len));
+                }
+            }
+            return stringBuffer.toString();
+        } catch (
+                MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
